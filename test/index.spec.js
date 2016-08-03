f5013dfe44ab29415d93b986f9ae9b033761d209
나무(Tree) <reaperes@coupang.com>
2016-08-03 14:53:37 +0900

    release 0.1.0

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,69 +1,21 @@
-// TODO : need to test later. It does not been completed yet.
-//var phantom = require('phantom');
-//var path = require('path');
-//
-//const indexHtmlPath = path.resolve(__dirname, './index.html');
-//
-//var phInstance = null;
-//var sitePage = null;
-//var prevBodyStyle = null;
-//
-//phantom
-//  .create()
-//  .then(instance => {
-//    phInstance = instance;
-//    return instance.createPage();
-//  })
-//  .then(page => {
-//    sitePage = page;
-//    return page.open(indexHtmlPath);
-//  })
-//  .then(status => {
-//    if (status !== 'success') {
-//      console.error('Fail to open index.html file');
-//      phInstance.exit();
-//    }
-//
-//    return waitUntilPageReady(sitePage);
-//  })
-//  .then(function () {
-//    return sitePage.evaluate(function () {
-//      return document.getElementsByTagName('body')[0];
-//    });
-//  })
-//  .then(function (body) {
-//    prevBodyStyle = body.style;
-//    return true;
-//  })
-//  .then(function () {
-//    return sitePage.evaluate(function () {
-//      //window.DynamicCss.appendCss('./a.css');
-//      return document.getElementsByTagName('body')[0];
-//    });
-//  })
-//  .then(function (body) {
-//    console.log(body);
-//    phInstance.exit();
-//  });
-//
-//function waitUntilPageReady(page) {
-//  return new Promise(function (res, rej) {
-//    function setTimeoutUntilPageReady() {
-//      setTimeout(function () {
-//        page
-//          .evaluate(function () {
-//            return document.readyState;
-//          })
-//          .then(readyState => {
-//            if (readyState !== 'complete') {
-//              setTimeoutUntilPageReady();
-//              return;
-//            }
-//            res();
-//          });
-//      });
-//    }
-//
-//    setTimeoutUntilPageReady();
-//  });
-//}
+var expect = chai.expect;
+
+describe('index.spec.js', function () {
+  it('appendCss should import css', function (done) {
+    this.timeout(5000);
+    var prevBackgroundColor = window.getComputedStyle(document.body).backgroundColor;
+    DynamicCss.appendCss('test/a.css');
+
+    setTimeout(function () {
+      var nextBackgroundColor = window.getComputedStyle(document.body).backgroundColor;
+      expect(prevBackgroundColor).to.not.equal(nextBackgroundColor);
+
+      DynamicCss.removeCss('test/a.css');
+      setTimeout(function () {
+        var lastBackgroundColor = window.getComputedStyle(document.body).backgroundColor;
+        expect(lastBackgroundColor).to.equal(prevBackgroundColor);
+        done();
+      }, 1000);
+    }, 1000);
+  })
+});
